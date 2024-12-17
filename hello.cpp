57b5822b714f0cea6bbceb1189e75dba2ae60715
Nowfil5 <nowfil.5r@gmail.com>
2024-12-17 12:01:51 +0530

    chore:demo for checking

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main(){
+    int a=6,n;
+    std::cout<<("I love to play Volleyball")<<std::endl;
+    std::cout<<("Enter a number")<<std::endl;
+    std::cin>>(n);
+    std::cout<<n;
+    return 0;
+ 
+}
